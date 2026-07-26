68741aa28ef66a26992392103e0f81e216811a4d
amirpashayarjanli-star <amirpasha.yarjanli@gmail.com>
2026-07-26 14:52:43 +0330

    prepare employer model relations

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Employer extends Model
 {
-    //
+
+    protected $fillable = [
+
+        'name',
+        'slug',
+        'mobile',
+        'phone',
+        'email',
+        'province',
+        'city',
+        'address',
+        'description',
+        'is_verified',
+        'is_active',
+
+    ];
+
+
+
+
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
 }
