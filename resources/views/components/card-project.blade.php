93e4d65832307de14e2e12e39004b08c8782eb34
amirpashayarjanli-star <amirpasha.yarjanli@gmail.com>
2026-07-27 12:40:00 +0330

    fix project card link

--- resources/views/components/card-project.blade.php
+++ resources/views/components/card-project.blade.php
@@ -1,81 +1,59 @@
 <div class="company-card project-card">
 
-
     <div class="project-image">
 
-
         <img
-            src="{{ asset($project['image']) }}"
-            alt="{{ $project['name'] }}">
-
+            src="{{ asset($project['image'] ?? 'images/project.jpg') }}"
+            alt="{{ $project['name'] ?? 'پروژه آسانسور' }}">
 
     </div>
 
 
-
-
     <h3>
-
-        {{ $project['name'] }}
-
+        {{ $project['name'] ?? 'پروژه بدون نام' }}
     </h3>
 
 
-
-
     <div class="company-info">
-
 
         <span>
 
             <i class="fa-solid fa-location-dot"></i>
 
-            {{ $project['city'] }}
+            {{ $project['city'] ?? 'نامشخص' }}
 
         </span>
-
 
 
         <span>
 
             <i class="fa-solid fa-building"></i>
 
-            {{ $project['type'] }}
+            {{ $project['type'] ?? 'پروژه آسانسور' }}
 
         </span>
 
-
     </div>
-
-
 
 
     <div class="project-status">
 
-
         <span></span>
 
-
         {{ $project['status'] ?? 'فعال' }}
-
 
     </div>
 
 
-
-
-    <a href="#"
-       class="company-profile-btn">
-
+    <a
+        href="{{ !empty($project['slug']) ? route('project.profile', $project['slug']) : route('projects.index') }}"
+        class="company-profile-btn">
 
         مشاهده پروژه
 
-
         <i class="fa-solid fa-arrow-left"></i>
 
-
     </a>
-
 
 
 </div>
