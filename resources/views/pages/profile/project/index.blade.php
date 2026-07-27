db25d7e14b23b07b5c36050e5629beda750e6b06
amirpashayarjanli-star <amirpasha.yarjanli@gmail.com>
2026-07-27 12:46:24 +0330

    add project profile page

---
+++ resources/views/pages/profile/project/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', $project->name ?? 'پروژه آسانسور')
+
+@section('content')
+
+<main>
+
+    <div class="container">
+
+        <h1>
+            {{ $project->name ?? 'پروژه آسانسور' }}
+        </h1>
+
+
+        @if($project->city)
+
+            <p>
+                شهر:
+                {{ $project->city }}
+            </p>
+
+        @endif
+
+
+        @if($project->description)
+
+            <p>
+                {{ $project->description }}
+            </p>
+
+        @else
+
+            <p>
+                توضیحات این پروژه هنوز ثبت نشده است.
+            </p>
+
+        @endif
+
+
+    </div>
+
+</main>
+
+@endsection
